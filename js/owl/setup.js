92f54fff6ace8134e95e595600c0c50632f318f9
Vitor Gonçalves Rodrigues <vitor7goncalves@outlook.com>
2021-01-06 11:51:07 -0300

    Aprimorando a interface do site clone

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -16,3 +16,21 @@ $('.owl-carousel').owlCarousel({
 })
 
 // $ significa que voce está usando o jquery
+var v = document.querySelector("video")
+v.play();
+
+var v2 = document.getElementsByClassName("video-trailler2")[0];
+
+document.querySelector(".botao2").addEventListener("click",function(){
+    document.querySelector(".filme-principal").classList.toggle("mini");
+    v.pause();
+    v2.play();
+    });
+
+document.querySelector(".close").addEventListener("click",function(){
+    document.querySelector(".filme-principal").classList.toggle("mini");
+    v.play();
+    v2.pause();
+    });
+
+        
